499eccc343f71add1dea0b5258266ac9ff6203e2
wwoj <wojcik.wojtek@yahoo.pl>
2020-06-01 11:11:27 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import ReactPDF from '@react-pdf/renderer';
+import ReactDOM from 'react-dom';
+import { PDFViewer } from '@react-pdf/renderer';
 function App() {
+ 
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +16,43 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<button onClick={printPDF}>Drukuj PDF</button>
+      
+      <PDFViewer>
+    <MyDocument />
+  </PDFViewer>
+  </header>
     </div>
   );
 }
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
 
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+); 
+function printPDF(){
+  ReactPDF.render(<MyDocument />, `example.pdf`);
+}
 export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
